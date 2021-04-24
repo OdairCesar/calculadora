daf91b3750a7514ed7053f705834a51f0d8cd658
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-04-24 05:27:54 -0300

    Terminado o visual da calculadora. E deixando preparado para fazer a logica computacional de como funcionará a calculadora

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,18 @@
 import React from 'react'
 import './Button.css'
 
-export default props => <button className='button'>{props.label}</button>
+export default props => {
+    let classes = 'button'
+    classes += props.operation ? ' operation' : ''
+    classes += props.doubleColumn ? ' doubleColumn' : ''
+    classes += props.operationB ? ' operationB' : ''
+    classes += props.doubleRow ? ' doubleRow' : ''
+
+    return(
+        <button 
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+                {props.label}
+        </button>
+    )
+}
