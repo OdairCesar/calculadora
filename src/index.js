65c7e07e88a3d5a76b7df5c5744c2f69d0747e3d
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-04-24 04:09:17 -0300

    Criando e implementando os botões da calculadora

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Calculator from'./main/calculator';
+import Calculator from'./main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
